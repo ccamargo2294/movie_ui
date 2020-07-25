915852ae927e2a14d72cc746261b9795f6afa173
Camilo Camargo <camilo.220294@gmail.com>
2020-07-25 05:24:05 -0500

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hi link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const spanElement = getByText(/Hi/i);
+  expect(spanElement).toBeInTheDocument();
 });
